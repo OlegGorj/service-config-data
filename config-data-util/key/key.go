29a5f6c6fc2df72ef7f255eabd217cde774cccf4
oleg-gorodnitchi <oleggorj@hotmail.com>
2019-09-06 15:46:19 -0400

    dev rounds

--- config-data-util/key/key.go
+++ config-data-util/key/key.go
@@ -1,11 +1,11 @@
 package key
 
 import (
-	_ "encoding/json"
+	"encoding/json"
 	"fmt"
 	_ "gopkg.in/src-d/go-billy.v4"
 	_ "github.com/oleggorj/service-config-data/config-data-util/memfilesystem"
-	_ "github.com/oleggorj/service-common-lib/common/logging"
+	log "github.com/oleggorj/service-common-lib/common/logging"
 )
 
 type Keys []Key
@@ -15,6 +15,25 @@ type Key struct {
 	Val string
 }
 
+func (keys *Keys) Init(jsonBuffer []byte) (Keys, error){
+	//fmt.Println("AFTER ", string(jsonBuffer))
+	keys_arr := make(map[string]interface{})
+	err := json.Unmarshal(jsonBuffer, &keys_arr)
+	if err != nil {
+	    log.Error(err)
+	}
+	var allkeys Keys
+	for j_key, j_value := range keys_arr {
+			var k Key
+			k.Key = j_key
+			k.Val = fmt.Sprintf("%s", j_value )
+			allkeys = append(allkeys, k)
+			//fmt.Println("key : ", k.Key, " value : ", k.Val)
+  }
+
+	return allkeys, nil
+}
+
 func (keys *Keys) Read(key string) (string, error){
 
 	for i := range *keys{
@@ -22,5 +41,5 @@ func (keys *Keys) Read(key string) (string, error){
 			return (*keys)[i].Val, nil
 		}
 	}
-	return "", fmt.Errorf("user not found")
+	return "", fmt.Errorf("key not found")
 }
