b91b7c1d3f241af8fe3148f04a9a86e7b3e0774d
oleg-gorodnitchi <oleggorj@hotmail.com>
2019-09-10 10:00:03 -0400

    dev rounds

---
+++ config-data-util/data_utility.go
@@ -0,0 +1,13 @@
+package config_data_util
+
+// TODO: Populating structs based on config_data package in config_structs.go file
+// TODO: Think about how you can refactor this into an Interface
+
+
+
+//type ConfigCRUD interface {
+//	Create(environment *Environment, data []byte)
+//	Read(environment *Environment, id string) (index int, err error)
+//	Update(environment *Environment, id string, data[]byte) (error)
+//	Delete(environment *Environment, id string) (error)
+//}
